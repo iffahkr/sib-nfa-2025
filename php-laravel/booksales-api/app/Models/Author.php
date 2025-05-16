a39dfa36ff9243e98958aa2126395f97712f98e9
Iffah <iffahkarimaah@gmail.com>
2025-05-16 23:20:48 +0700

    remove unused comments

--- php-laravel/booksales-api/app/Models/Author.php
+++ php-laravel/booksales-api/app/Models/Author.php
@@ -7,37 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     protected $genres = 'genres';
-    
-    // private $authors = [
-    //     [
-    //         'name' => 'Tere Liye',
-    //         'photo' => 'tere_liye.jpg',
-    //         'bio' => 'Penulis novel asal Indonesia dengan buku populernya Hujan.'
-    //     ],
-    //     [
-    //         'name' => 'Mark Manson',
-    //         'photo' => 'mark_manson.jpg',
-    //         'bio' => 'Penulis asal Amerika dengan buku populernya Sebuah Seni untuk Bersikap Bodo Amat.'
-    //     ],
-    //     [
-    //         'name' => 'Masashi Kishimoto',
-    //         'photo' => 'masashi_kishimoto.jpg',
-    //         'bio' => 'Penulis asal Jepang yang terkenal dengan karya populernya komik Naruto.'
-    //     ],
-    //     [
-    //         'name' => 'James Clear',
-    //         'photo' => 'james_clear.jpg',
-    //         'bio' => 'Penulis asal Amerika yang terkenal dengan karya populernya Atomic Habits.'
-    //     ],
-    //     [
-    //         'name' => 'Rintik Sedu',
-    //         'photo' => 'rintik_sedu.jpg',
-    //         'bio' => 'Penulis Novel asal Indonesia yang terkenal dengan karyanya bergenre galau.'
-    //     ]
-    // ];
-
-    // public function getAuthors() {
-    //     return $this->authors;
-    // }
 
 }
--- php-laravel/booksales-api/app/Models/Genre.php
+++ php-laravel/booksales-api/app/Models/Genre.php
@@ -7,32 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Genre extends Model
 {
     protected $table = 'genres';
-    
-    // private $genres = [
-    //     [
-    //         'name' => 'Fiksi',
-    //         'description' => 'Sebuah genre berdasarkan imajinasi dan kejadian yang tidak nyata.'
-    //     ],
-    //     [
-    //         'name' => 'Motivasi',
-    //         'description' => 'Sebuah genre yang mendorong perubahan diri ke arah yang lebih baik.'
-    //     ],
-    //     [
-    //         'name' => 'Komik',
-    //         'description' => 'Sebuah genre animasi kartun dengan tulisan tangan.'
-    //     ],
-    //     [
-    //         'name' => 'Non-Fiksi',
-    //         'description' => 'Sebuah genre berdasarkan dengan fakta dan kejadian nyata.'
-    //     ],
-    //     [
-    //         'name' => 'Fantasi',
-    //         'description' => 'Sebuah genre yang mengandung karangan majik dan mistis.'
-    //     ]
-    // ];
-
-    // public function getGenres() {
-    //     return $this->genres;
-    // }
 
 }
